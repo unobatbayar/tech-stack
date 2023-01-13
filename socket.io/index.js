6360a3dd024d610f3fc3f90bae58898a1c18e3ab
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2023-01-13 10:55:03 +0900

    fix

--- socket.io/index.js
+++ socket.io/index.js
@@ -16,6 +16,12 @@ io.on('connection', (socket) => {
     });
   });
 
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
