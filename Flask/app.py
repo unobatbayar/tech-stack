5055cdaaa305e260507e63370714159eb35ff48f
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2024-07-02 23:55:11 +0800

    feat: add request to url

--- Flask/app.py
+++ Flask/app.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    name = request.args.get("name")
+    return render_template("index.html", name)
