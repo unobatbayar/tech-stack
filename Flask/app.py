8ef1b746f56cc8a7f130635622e2d789daf2a6ef
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2024-07-12 23:14:59 +0800

    feat: add form

--- Flask/app.py
+++ Flask/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", name = request.args.get("name", "world"))
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name = request.args.get("name", "world"))
