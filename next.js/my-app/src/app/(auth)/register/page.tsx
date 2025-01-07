746f5fdbeb4e8a254319a61dca5891037f67fe18
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2025-01-08 08:40:26 +0900

    feat: learn dynamic routing, logical grouping

---
+++ next.js/my-app/src/app/(auth)/register/page.tsx
@@ -0,0 +1,3 @@
+﻿export default function Login() {
+    return <h1>Register</h1>;
+}
---
+++ next.js/my-app/src/app/products/[id]/page.tsx
@@ -0,0 +1,6 @@
+﻿
+export default async function Product({ params }: { params: { id: string } }) {
+
+    const { id } = await params;
+    return <h1>Product: {id}</h1>;
+}
