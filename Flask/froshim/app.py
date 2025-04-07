a6108d785590d22630951d504d3266c21f6624d1
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2025-04-07 17:06:38 +0900

    feat: in-memory storing registrants

--- Flask/froshim/app.py
+++ Flask/froshim/app.py
@@ -1,19 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
   
+
+sports = [ "Basketball", "Soccer", "Ultimate Frisbee"]
+
+REGISTRANTS = {}
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=sports, registrants=REGISTRANTS)
 
 
 @app.route("/register", methods=["POST"])
 def register():
     name = request.form.get("name")
+
+    if not name:
+        return render_template("error.html", error_message="No name found")
     sport = request.form.get("sport")
-    if name and sport:
-        return render_template("success.html")
-    return render_template("failure.html")
+    if sport not in sports:
+        return render_template("error.html", error_message="No sport found")
+
+    REGISTRANTS[name] = sport
+
+    return render_template("success.html")
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
+
+# Define a route
+@app.route('/weather')
+def weather():
+    # Create a Python dictionary with weather data
+    weather_data = {
+        'city': 'Tokyo',
+        'temperature': 22,
+        'unit': 'Celsius',
+        'condition': 'Clear'
+    }
+    
+    # Return the dictionary as a JSON response
+    return jsonify(weather_data)
