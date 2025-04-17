a300b5825e1ae88d94a6e89d52f3290310e896d0
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2025-04-17 14:18:31 +0900

    feat: clean start

---
+++ FastAPI/metrics/backend/models.py
@@ -0,0 +1,14 @@
+﻿from datetime import datetime
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base() 
+
+class UserModel(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    birth = Column(DateTime)
+    created = Column(DateTime, default=datetime.now)
